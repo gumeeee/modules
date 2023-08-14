63e6b3547ca55b4a208b8fa5d0b33d2d67496c5e
Guilherme Moura <gaxco123@gmail.com>
2023-08-14 15:54:02 -0300

    Primeiro commit do repositorio

---
+++ index.js
@@ -0,0 +1,13 @@
+import { areaQuadrado, perimetroQuadrado, IMC } from "./quadrado.js";
+import numeroAleatorio from "./numeroAleatorio.js";
+import Circulo from "./Circulo.js";
+
+console.log(areaQuadrado(4));
+console.log(perimetroQuadrado(5));
+console.log(IMC(62, 1.75));
+
+console.log(numeroAleatorio());
+
+console.log(Circulo.area(5));
+console.log(Circulo.circunferencia(5));
+console.log(Circulo.aleatorio());
---
+++ quadrado.js
@@ -0,0 +1,11 @@
+export function areaQuadrado(l) {
+  return l * l;
+}
+
+export function perimetroQuadrado(l) {
+  return 4 * l;
+}
+
+export function IMC(peso, altura) {
+  return peso / (altura * altura);
+}
